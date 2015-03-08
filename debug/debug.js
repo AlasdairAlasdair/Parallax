de33c5939e8dabbe3b1bd3f315a6ada97acb97f7
ali <Ali@macbook.local>
2015-03-08 21:08:57 +0000

    Hackle add a colour toggle

--- debug/debug.js
+++ debug/debug.js
@@ -1,6 +1,13 @@
-var debugInput = document.querySelector("input");
+var debugInput = document.querySelector("#debug-checkbox");
 function updateDebugState() {
     document.body.classList.toggle('debug-on', debugInput.checked);
 }
 debugInput.addEventListener("click", updateDebugState);
 updateDebugState();
+
+var coloursInput = document.querySelector("#colours-checkbox");
+function updateColoursState() {
+    document.body.classList.toggle('colours-off', coloursInput.checked);
+}
+coloursInput.addEventListener("click", updateColoursState);
+updateColoursState();
